6896ea5f6e761d47f493b2b7bf019398a42a25fb
unknown <1415176717@qq.com>
2022-11-28 14:09:10 -0600

    update solver

--- Cosserat_rods.py
+++ Cosserat_rods.py
@@ -3,9 +3,12 @@ from matplotlib import pyplot as plt
 
 # Variables on elements
 class CosseratRod():
-  def __init__(self, number_of_elements, total_length, density, radius, direction, normal, youngs_modulus, poisson_ratio):
+  def __init__(self, number_of_elements, total_length, density, radius, direction, normal, youngs_modulus, poisson_ratio, dt, total_time):
     #direction must be a unit vector
     self.direction = direction / np.linalg.norm(direction)
+
+    #callback parameters
+    self.callback_params = {} #an empty dictionary that stores data for each time steps
 
     #properties for whole rod
     self.youngs_modulus = youngs_modulus
@@ -16,14 +19,16 @@ class CosseratRod():
     self.total_length = total_length
     self.density = density
     self.radius = radius
+    self.dt = dt
+    self.total_time = total_time
+    self.time = np.arange(0, total_time, dt)
 
     # Element scalars
     self.reference_lengths = np.ones((self.n_elements))* self.total_length/self.n_elements # Here I took total rod length as 1
-    self.current_lengths = self.reference_length.copy() #initially, current length is initial length
+    self.current_lengths = self.reference_lengths.copy() #initially, current length is initial length
     self.area = np.pi * self.radius**2 #initial
     self.current_area = self.area * np.ones((self.n_elements))
-    self.current_radius = self.radius * np.ones((self.n_elements))
-    self.element_volume = self.area * self.current_lengths
+    self.element_volume = self.area * self.reference_lengths[0] #volume is not changing
     self.element_mass = self.element_volume * density
     self.element_dilation = np.ones((self.n_elements)) #initialize first, update later
     #moment of area
@@ -31,38 +36,49 @@ class CosseratRod():
         #moment of inertia
     self.J = np.zeros((self.n_elements))
 
-    for element in range(self.n_elements):
-        self.I[:, element] = self.current_area[element]**2/(4 * np.pi) * np.array([1, 1, 2])
-        self.J[element] = self.current_length[element]**2 * self.element_mass[element] / 2
+    #initialize accelerations and velocities
+    self.acceleration = np.zeros([self.n_elements])
+    self.angular_acceleration = np.zeros([self.n_elements])
+    self.velocitiy = np.zeros([self.n_elements])
+    self.angular_velocity = np.zeros([self.n_elements])
 
     #nodal mass
     self.mass = np.zeros((self.n_elements + 1))
     self.mass[:-1] += 0.5 * self.element_mass
     self.mass[1:] += 0.5 * self.element_mass
-    
+
+    for element in range(self.n_elements):
+        self.I[:, element] = self.current_area[element]**2/(4 * np.pi) * np.array([1, 1, 2])
+        self.J[element] = self.reference_lengths[element]**2 / 4 * self.mass[element] + \
+                          self.reference_lengths[element]**2 / 4 * self.mass[element + 1] #point mass on two ends of each elements
+
+
+
+
     #nodal positions
+    self.angular_position = np.zeros((3, self.n_elements))
     self.position = np.zeros((3, self.n_elements + 1))
-    discretize_length = np.linspace(0, self.total_length, self.n_elements + 1)
+    discretize_lengths = np.linspace(0, self.total_length, self.n_elements + 1)
     for node in range(self.n_elements + 1):
-        self.position[:, node] =  discretize_length[node] * direction
+        self.position[:, node] =  discretize_lengths[node] * direction
 
     # Element vectors
-    self.directors = np.zeros((3,3, self.n_elements))
+    self.Q = np.zeros((3,3, self.n_elements)) #directors
     self.tangents = self.position[:, 1:] - self.position[:, :-1]
     self.tangents /= np.linalg.norm(self.tangents, axis=0, keepdims=True)
     for idx in range(self.n_elements):
         self.d1 = normal 
         self.d3 = direction
         self.d2 = np.cross(self.d3, self.d1) # binormal
-        self.directors[0, :, idx] = self.d1 # d1
-        self.directors[1, :, idx] = self.d2 # d2
-        self.directors[2, :, idx] = self.d3 # d3
+        self.Q[0, :, idx] = self.d1 # d1
+        self.Q[1, :, idx] = self.d2 # d2
+        self.Q[2, :, idx] = self.d3 # d3
 
     self.sigma = np.zeros((3, self.n_elements)) #shear strain vector, initialize first, update later
     
     # Element matrix
-    self.element_bend_matrix = np.zeros((3, 3, self.n_elements))
-    self.shear_matrix = np.zeros((3, 3, self.n_elements))
+    self.element_B = np.zeros((3, 3, self.n_elements))
+    self.S = np.zeros((3, 3, self.n_elements))
     
     self.alpha = 4/3
     for element in range(self.n_elements):
@@ -73,82 +89,238 @@ class CosseratRod():
         S2 = self.alpha * self.shear_modulus * self.current_area[element]
         S3 = self.youngs_modulus * self.current_area[element]
 
-        self.element_bend_matrix[:, :, element] = np.diag([B1, B2, B3])
-        self.shear_matrix[:, :, element] = np.diag([S1, S2, S3])
+        self.element_B[:, :, element] = np.diag([B1, B2, B3])
+        self.S[:, :, element] = np.diag([S1, S2, S3])
+
+    #external force and torque
+    self.external_force = np.zeros([1, self.n_nodes])
+    self.external_torque = np.zeros([1, self.n_elements])
     
     # Voronoi scalars
-    self.reference_voronoi_length = (self.reference_lengths[1:] - self.reference_lengths[:-1]) / 2
-    self.current_voronoi_length = self.reference_voronoi_length.copy()
+    self.reference_voronoi_lengths = (self.reference_lengths[1:] - self.reference_lengths[:-1]) / 2
+    self.current_voronoi_lengths = self.reference_voronoi_lengths.copy()
     self.voronoi_dilation = np.ones((self.n_elements)) #initialize first, update later
     # Voronoi vectors
     self.kappa = np.zeros((3, self.n_voronoi)) #initialize but update later
     # Voronoi matrix
-    self.bend_matrix = np.zeros((3,3,self.n_voronoi))
+    self.B = np.zeros((3,3,self.n_voronoi))
 
     for voronoi in range(self.n_voronoi):
-        self.bend_matrix[:,:,voronoi] = (self.element_bend_matrix[:,:, voronoi + 1] * self.reference_lengths[voronoi + 1] \
-         + self.element_bend_matrix[: ,: , voronoi] * self.reference_lengths[voronoi]) / (2 * self.reference_voronoi_length[voronoi])
+        self.B[:,:,voronoi] = (self.element_B[:,:, voronoi + 1] * self.reference_lengths[voronoi + 1] \
+         + self.element_B[: ,: , voronoi] * self.reference_lengths[voronoi]) / (2 * self.reference_voronoi_lengths[voronoi])
+
+def _batch_matmul(first_matrix_collection, second_matrix_collection):
+    """
+    This is batch matrix matrix multiplication function. Only batch
+    of 3x3 matrices can be multiplied.
+    Parameters
+    ----------
+    first_matrix_collection
+    second_matrix_collection
+
+    Returns
+    -------
+    Notes
+    Microbenchmark results showed that for a block size of 200,
+    %timeit np.einsum("ijk,jlk->ilk", first_matrix_collection, second_matrix_collection)
+    8.45 µs ± 18.6 ns per loop
+    This version is
+    2.13 µs ± 1.01 µs per loop
+    """
+    blocksize = first_matrix_collection.shape[2]
+    output_matrix = np.zeros((3, 3, blocksize))
+
+    for i in range(3):
+        for j in range(3):
+            for m in range(3):
+                for k in range(blocksize):
+                    output_matrix[i, m, k] += (
+                        first_matrix_collection[i, j, k]
+                        * second_matrix_collection[j, m, k]
+                    )
+
+    return output_matrix
+
+def _batch_matvec(matrix_collection, vector_collection):
+    """
+    This function does batch matrix and batch vector product
+
+    Parameters
+    ----------
+    matrix_collection
+    vector_collection
+
+    Returns
+    -------
+    Notes
+    -----
+    Benchmark results, for a blocksize of 100, using timeit
+    Python einsum: 4.27 µs ± 66.1 ns per loop
+    This version: 1.18 µs ± 39.2 ns per loop
+    """
+    blocksize = vector_collection.shape[1]
+    print(blocksize, "of vector")
+    output_vector = np.zeros((3, blocksize))
+
+    for i in range(3):
+        for j in range(3):
+            for k in range(blocksize):
+                output_vector[i, k] += (
+                    matrix_collection[i, j, k] * vector_collection[j, k]
+                )
+    return output_vector
+
+def _batch_cross(first_vector_collection, second_vector_collection):
+    """
+    This function does cross product between two batch vectors.
+
+    Parameters
+    ----------
+    first_vector_collection
+    second_vector_collection
+
+    Returns
+    -------
+    Notes
+    ----
+    Benchmark results, for a blocksize of 100 using timeit
+    Python einsum: 14 µs ± 8.96 µs per loop
+    This version: 1.18 µs ± 141 ns per loop
+    """
+    blocksize = first_vector_collection.shape[1]
+    output_vector = np.empty((3, blocksize))
+
+    for k in range(blocksize):
+        output_vector[0, k] = (
+            first_vector_collection[1, k] * second_vector_collection[2, k]
+            - first_vector_collection[2, k] * second_vector_collection[1, k]
+        )
+
+        output_vector[1, k] = (
+            first_vector_collection[2, k] * second_vector_collection[0, k]
+            - first_vector_collection[0, k] * second_vector_collection[2, k]
+        )
+
+        output_vector[2, k] = (
+            first_vector_collection[0, k] * second_vector_collection[1, k]
+            - first_vector_collection[1, k] * second_vector_collection[0, k]
+        )
+
+    return output_vector
 
 def force_rule(self):
-    pass
     # according to cosserate therory, solve for the linear and angular acceleartions
+    #first solve for acceleration:
+    QT_S_sigma_over_e = _batch_matvec(_batch_matmul(self.Q.T, self.S), self.sigma) / self.element_dilation #batch transformation
+    acceleration = (delta_h(QT_S_sigma_over_e) + self.external_force) / self.mass
+    
+    #then solve for angular acceleration:
+    B_kappa_over_epsilon_cube = _batch_matmul(self.B, self.kappa) / self.voronoi_dilation ** 3
+    J_over_e = self.J / self.element_dilation
+    kappa_cross_B_kappa = _batch_cross(self.kappa, _batch_matvec(self.B, self.kappa))
+    kappa_cross_B_kappa_over_epsilon_cube_D = (kappa_cross_B_kappa / self.voronoi_dilation ** 3) * self.reference_voronoi_length
+    Q_t_cross_S_sigma_l = _batch_cross(_batch_matvec(self.Q, self.tangents) , _batch_matvec(self.S, self.sigma)) * self.reference_length
+    angular_acceleration = (delta_h(B_kappa_over_epsilon_cube) + alpha_h(kappa_cross_B_kappa_over_epsilon_cube_D) + Q_t_cross_S_sigma_l\
+         + self.external_torque) / J_over_e
 
+    return acceleration, angular_acceleration
+   
 def update(self):
-    pass
-    #update current force
+    #calculate accelerations
+    self.acceleration, self.angular_acceleration = self.force_rule()
+    #update current force by specific force class
+    #update current torque by specific torque class
 
-    #update current postion
+    #update current director before we update the angular velocity since this involves half step
+    half_step_angular_velocity = self.angular_velocity + self.angular_acceleration * dt / 2
+    for element in range(self.n_elements):
+        Q_t_half_dt = np.exp(-self.dt/2 + self.angular_velocity[element]) * self.Q[:,:,element]
+        self.Q[:,:,:element] = np.exp(-self.dt/2 * half_step_angular_velocity[element]) * Q_t_half_dt[element]
 
     #update current velocity
+    #update current postion
+    self.position, self.velocity = position_verlet(self.dt, self.position, self.velocity, self.acceleration)
+
+    #update current angular velocity. Unfortunatly there is no way to get half step acceleration
+    self.angular_position, self.angular_velocity = position_verlet(self.dt, self.angular_position, self.angular_velocity, self.angular_acceleration)
+    
+    
+    
 
     #update current length
+    #update current tangent
+    self.tangents = self.position[:, 1:] - self.position[:, :-1]
+    self.current_lengths = np.linalg.norm(self.tangents, axis=0, keepdims=True)
+    self.tangents /= self.current_lengths
 
     #update current dilation
+    self.element_dilation = self.current_lengths / self.reference_lengths
 
-    #update current tangent
-
-    #update current shear strain
-
-    #update current angular velocity
-
-    #update current director
-
-    #update current area
-
-    #update current moment of inertia
-
-    #update current moment of area
-
-    #update current element bending matrix
-
-    #update current element shear matrix
-
-    #update current external moment
-
-    #update current voronoi length
-
+    #update current voronoi lengths
+    self.current_voronoi_lengths = (self.current_lengths[1:] - self.current_lengths[:-1]) / 2
+    
     #update current voronoi dilation
+    self.voronoi_dilation = self.current_voronoi_lengths / self.reference_voronoi_lengths
+    
+    
+    for element in range(self.n_elements):
+        #update current shear strain sigma
+        d3 = self.Q[2, :, element]
+        self.sigma[element] = self.Q[:,:,element] * (self.element_dilation[element] * self.tangents - d3)
+    
+        #update current area
+        # self.current_area = self.current_area / self.element_dilation
 
-    #update current R = Q @ Q.T
+        #update current moment of area
+        # self.I[element] = self.I[element] / self.element_dilation[element] ** 2
+        
+        # #update current moment of inertia
+        # self.J[element] = self.current_lengths[element]**2 / 4 * self.mass[element] + \
+        #                   self.current_lengths[element]**2 / 4 * self.mass[element + 1] #point mass on two ends of each elements
+        
+        # #update current element bending matrix
+        # self.B[:,:,element] = self.B[:,:,element] / self.element_dilation[element] ** 2
 
-    #update current theta = np.acos((np.trace(R) - 1) / 2)
+        # #update current element shear matrix
+        # self.S[:,:,element] = self.S[:,:,element] / self.element_dilation[element]
 
-    #update current voronoi kappa
+    for voronoi in range(self.n_voronoi):
+        #update current R = Q @ Q.T
+        R = self.Q[:,:,voronoi + 1] @ self.Q[:,:,voronoi].T
 
-    #update current bend matrix
+        #update current theta = np.acos((np.trace(R) - 1) / 2)
+        theta = np.acos((np.trace(R) - 1) / 2)
 
+        #update skew_symmetric matrix U and vector u associated with it
+        U = R - R.T
+        u = np.array([U[2,1], -U[2,0], U[1,0]])
+        #update log_Q_QT
+        if theta == 0:
+            log_Q_QT = 0
+        else:
+            log_Q_QT = theta / (2 * np.sin(theta)) * u
+
+        #update current kappa
+        self.kappa[voronoi] = log_Q_QT / self.reference_voronoi_length
+
+def make_callback(self):
+
+    
+    self.callback_params["position"].append(self.position_collection.copy())
+    self.callback_params["velocity"].append(self.velocity_collection.copy())
+    self.callback_params["curvature"].append(self.kappa.copy())
+
+    return
 
 # Difference operator (Delta^h operator in discreatized cosserat rod equations)
-def modified_diff(t_x):
+def delta_h(t_x):
     """ Modified trapezoidal integration"""
     # Pads a 0 at the end of an array
-    temp = np.pad(t_x, (0,1), 
-                  'constant', 
-                  constant_values=(0,0)) # Using roll calculate the diff (ghost node of 0)
+    temp = np.pad(t_x, (0,1), 'constant', constant_values=(0,0)) # Using roll calculate the diff (ghost node of 0)
     return (temp - np.roll(temp, 1))[:-1, :]
 
 # Modified trapezoidal integration (A^h operator in discreatized cosserat rod equations)
-def modified_trapz(t_x):
+def alpha_h(t_x):
   """ Modified trapezoidal integration"""
   # Pads a 0 at the start of an array
   temp = np.pad(t_x, (1,0), 'constant', constant_values=(0,0))
@@ -156,28 +328,42 @@ def modified_trapz(t_x):
   return (0.5*(temp + np.roll(temp, -1)))[1:, :]
 
 def position_verlet(dt, x, v, force_rule):
-        """Does one iteration/timestep using the Position verlet scheme
-        
-        Parameters
-        ----------
-        dt : float
-            Simulation timestep in seconds
-        x : float/array-like
-            Quantity of interest / position of COM
-        v : float/array-like
-            Quantity of interest / velocity of COM
-        force_rule : ufunc
-            A function, f, that takes one argument and
-            returns the instantaneous forcing
+    """Does one iteration/timestep using the Position verlet scheme
+    
+    Parameters
+    ----------
+    dt : float
+        Simulation timestep in seconds
+    x : float/array-like
+        Quantity of interest / position of COM
+    v : float/array-like
+        Quantity of interest / velocity of COM
+    force_rule : ufunc
+        A function, f, that takes one argument and
+        returns the instantaneous forcing
 
-        Returns
-        -------
-        x_n : float/array-like
-            The quantity of interest at the Next time step
-        v_n : float/array-like
-            The quantity of interest at the Next time step
-        """
-        temp_x = x + 0.5*dt*v
-        v_n = v + dt * force_rule(temp_x)
-        x_n = temp_x + 0.5 * dt * v_n
-        return x_n, v_n
+    Returns
+    -------
+    x_n : float/array-like
+        The quantity of interest at the Next time step
+    v_n : float/array-like
+        The quantity of interest at the Next time step
+    """
+    temp_x = x + 0.5*dt*v
+    v_n = v + dt * force_rule(temp_x)
+    x_n = temp_x + 0.5 * dt * v_n
+    return x_n, v_n
+
+
+n_elements = 1
+length = 1
+density = 1000
+radius = 0.1
+direction = np.array([0, 0, 1])
+normal = np.array([0, 1, 0])
+youngs_modulus = 10e6
+poisson_ratio = 0.5
+dt = 0.001
+total_time = 1
+rod = CosseratRod(number_of_elements=n_elements, total_length=length, density=density, radius=radius, direction=direction,
+    normal=normal, youngs_modulus=youngs_modulus, poisson_ratio=poisson_ratio, dt=dt, total_time= total_time)
